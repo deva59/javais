7e3f66eb80134de78ce8245577d301dbe41eea7f
deva59 <devendrashimpi59@gmail.com>
2024-01-11 19:22:34 +0530

    update thiscode

--- gfg.java
+++ gfg.java
@@ -1,10 +1,10 @@
-import java.util.*;
-public class gfg {
+
+ class gfg {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-            int a = sc.nextInt();
-             int b = sc.nextInt();
+     
+            int a =20;
+             int b = 34;
              int sum= a+b;
             System.out.println(sum);
 
